46afceecfd6482cf724268f89c2133842317967c
Rooklyn Kline <rookkline99@gmail.com>
2022-06-14 09:26:42 -0400

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="tf14_cli",
-    version="0.0.1",
+    version="0.0.2",
     description="This contains a CLI for AWS terraform",
     author="Rooklyn Kline",
     author_email="",
